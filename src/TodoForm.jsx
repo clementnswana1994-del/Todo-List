ba97335a76dffbce9e7f2e3ae679755e9768a942
clementnswana1994-del <clementnswana1994@gmail.com>
2025-11-11 23:36:11 -0500

    third commit

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -36,7 +36,7 @@ function TodoForm(props) {
             className='todo-input edit'
           />
           <button onClick={handleSubmit} className='todo-button edit'>
-            Update
+            Edit
           </button>
         </>
       ) : (
@@ -50,7 +50,7 @@ function TodoForm(props) {
             ref={inputRef}
           />
           <button onClick={handleSubmit} className='todo-button'>
-            Add todo
+            Add
           </button>
         </>
       )}
